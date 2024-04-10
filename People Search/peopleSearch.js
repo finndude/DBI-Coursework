afb5b1377d7bf4ec80a431d47715b527c3ed859f
psyfw2 <psyfw2@exmail.nottingham.ac.uk>
2024-04-10 18:53:53 +0100

    Files Created and Setup

--- People Search/peopleSearch.js
+++ People Search/peopleSearch.js
@@ -2,8 +2,9 @@ import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+
 
 const supabase = createClient('https://adbhzvvfknicxaxnowok.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkYmh6dnZma25pY3hheG5vd29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI3NjYyMTgsImV4cCI6MjAyODM0MjIxOH0.spZ9Df831dq8DIglVvdsbnn6ygk2YL1q7ecG8MvH_zw')
 
-async function fetchData() {
-    const {data, error} = await supabase.from('DBI Coursework').select();
+async function fetchData() 
+{
+    const {data, error} = await supabase.from('People').select();
     console.log('Fetched data:', data);
 }
 
---
+++ Vehicle Search/vehicleSearch.js
@@ -0,0 +1,11 @@
+import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+const supabase = createClient('https://adbhzvvfknicxaxnowok.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkYmh6dnZma25pY3hheG5vd29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI3NjYyMTgsImV4cCI6MjAyODM0MjIxOH0.spZ9Df831dq8DIglVvdsbnn6ygk2YL1q7ecG8MvH_zw')
+
+async function fetchData() 
+{
+    const {data, error} = await supabase.from('Vehicles').select();
+    console.log('Fetched data:', data);
+}
+
+fetchData();
