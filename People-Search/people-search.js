70810d58d4f15be8aecd7c7c4f335beb2e994735
psyfw2 <psyfw2@exmail.nottingham.ac.uk>
2024-04-21 20:02:09 +0100

    Test

--- People-Search/people-search.js
+++ People-Search/people-search.js
@@ -140,8 +140,7 @@ document.getElementById("peopleSearchForm").addEventListener("submit", async (ev
             searchResults.forEach(person => // Loop through each search result and display it
             {
                 const personInfo = document.createElement("p");
-                personInfo.textContent = `Name: ${person.Name}, Address: ${person.Address}, DOB: ${person.DOB}, License Number: ${person.LicenseNumber}, Expiry Date: ${person.ExpiryDate}`;
-                personInfo.innerHTML = `<strong>Name: </strong>${person.Name}, <strong>Address: </strong>${person.Address}, <strong>DOB: </strong>${person.DOB}, <strong>License Number: </strong>${person.LicenseNumber}, <strong>Expiry Date: </strong>${person.ExpiryDate}`;
+                personInfo.innerHTML = `<p>Name: ${person.Name}, Address: ${person.Address}, DOB: ${person.DOB}, License Number: ${person.LicenseNumber}, Expiry Date: ${person.ExpiryDate}</p>`;
                 results.appendChild(personInfo);
             });
 
@@ -186,8 +185,7 @@ document.getElementById("peopleSearchForm").addEventListener("submit", async (ev
             searchResults.forEach(person => // Loop through each search result and display it
             {
                 const personInfo = document.createElement("p");
-                personInfo.textContent = `Name: ${person.Name}, Address: ${person.Address}, DOB: ${person.DOB}, License Number: ${person.LicenseNumber}, Expiry Date: ${person.ExpiryDate}`;
-                personInfo.innerHTML = `<strong>Name: </strong>${person.Name}, <strong>Address: </strong>${person.Address}, <strong>DOB: </strong>${person.DOB}, <strong>License Number: </strong>${person.LicenseNumber}, <strong>Expiry Date: </strong>${person.ExpiryDate}`;
+                personInfo.innerHTML = `<p>Name: ${person.Name}, Address: ${person.Address}, DOB: ${person.DOB}, License Number: ${person.LicenseNumber}, Expiry Date: ${person.ExpiryDate}</p>`;
                 results.appendChild(personInfo);
             });
 
