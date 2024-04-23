83767af14a665adfa4050c6d1680d713d44da062
psyfw2 <psyfw2@exmail.nottingham.ac.uk>
2024-04-23 12:23:12 +0100

    Test

--- People-Search/people-search.js
+++ People-Search/people-search.js
@@ -139,14 +139,10 @@ document.getElementById("peopleSearchForm").addEventListener("submit", async (ev
         {
             searchResults.forEach(person => // Loop through each search result and display it
             {
-                const personDiv = document.createElement("div"); // Create a new div for each search result
-                personDiv.classList.add("person-result");
-
                 const personInfo = document.createElement("p");
                 personInfo.textContent = `Name: ${person.Name}, Address: ${person.Address}, DOB: ${person.DOB}, License Number: ${person.LicenseNumber}, Expiry Date: ${person.ExpiryDate}`;
-                personDiv.appendChild(personInfo);
-
-                results.appendChild(personDiv); // Append the personDiv to the .results div
+                personInfo.innerHTML = `<strong>Name: </strong>${person.Name}, <strong>Address: </strong>${person.Address}, <strong>DOB: </strong>${person.DOB}, <strong>License Number: </strong>${person.LicenseNumber}, <strong>Expiry Date: </strong>${person.ExpiryDate}`;
+                results.appendChild(personInfo);
             });
 
             document.getElementById("message").textContent = "Search successful";
@@ -189,14 +185,10 @@ document.getElementById("peopleSearchForm").addEventListener("submit", async (ev
         {
             searchResults.forEach(person => // Loop through each search result and display it
             {
-                const personDiv = document.createElement("div"); // Create a new div for each search result
-                personDiv.classList.add("person-result");
-
                 const personInfo = document.createElement("p");
                 personInfo.textContent = `Name: ${person.Name}, Address: ${person.Address}, DOB: ${person.DOB}, License Number: ${person.LicenseNumber}, Expiry Date: ${person.ExpiryDate}`;
-                personDiv.appendChild(personInfo);
-
-                results.appendChild(personDiv); // Append the personDiv to the .results div
+                personInfo.innerHTML = `<strong>Name: </strong>${person.Name}, <strong>Address: </strong>${person.Address}, <strong>DOB: </strong>${person.DOB}, <strong>License Number: </strong>${person.LicenseNumber}, <strong>Expiry Date: </strong>${person.ExpiryDate}`;
+                results.appendChild(personInfo);
             });
 
             document.getElementById("message").textContent = "Search successful";
